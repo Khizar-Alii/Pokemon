f71a764f7d64d4efdd08c70864fdabdc186ac2bf
Khizar Ali <khizibutt123@gmail.com>
2023-05-14 11:14:15 +0500

    Making navbar responsive

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Pokemon">
     <React.StrictMode>
       <App />
     </React.StrictMode>
